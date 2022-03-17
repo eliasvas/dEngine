8c52ae9da43fa40e681d835ce29be40eb7f182c4
EliasVas <vaselas99@gmail.com>
2022-03-17 10:15:54 +0200

    swapchain support

--- src/dgfx.c
+++ src/dgfx.c
@@ -6,6 +6,10 @@
 
 dgDevice dd;
 
+//NOTE(ilias): This is UGLY AF!!!!
+extern dWindow main_window;
+
+
 #define VK_CHECK(x)                                                 \
 	do                                                              \
 	{                                                               \
@@ -75,7 +79,7 @@ b32 dg_create_instance(dgDevice *ddev) {
 	return TRUE;
 }
 
-typedef struct dQueueFamilyIndices
+typedef struct dgQueueFamilyIndices
 {
     u32 graphics_family;
     //because we cant ifer whether the vfalue was initialized correctly or is garbage
@@ -83,14 +87,14 @@ typedef struct dQueueFamilyIndices
     
     u32 present_family;
     u32 present_family_found;
-}dQueueFamilyIndices;
+}dgQueueFamilyIndices;
 
 #define DG_QUEUE_FAMILY_MAX 32
 
 //@TODO(ilias) vkGetPHysicalDeviceQueueFamilyProperties doesn't work with static queue_families
-static dQueueFamilyIndices dg_find_queue_families(VkPhysicalDevice device)
+static dgQueueFamilyIndices dg_find_queue_families(VkPhysicalDevice device)
 {
-	dQueueFamilyIndices indices = {0};
+	dgQueueFamilyIndices indices = {0};
 
 	u32 queue_family_count = 1;
 	VkQueueFamilyProperties queue_families[DG_QUEUE_FAMILY_MAX];
@@ -143,10 +147,52 @@ static u32 dg_check_device_extension_support(VkPhysicalDevice device)
 	free(available_extensions);
     return TRUE;
 }
-
-static u32 is_device_suitable(VkPhysicalDevice device)
+typedef struct dgSwapChainSupportDetails
 {
-	dQueueFamilyIndices indices = dg_find_queue_families(device);
+    VkSurfaceCapabilitiesKHR capabilities;
+    VkSurfaceFormatKHR *formats;
+    u32 format_count;
+    VkPresentModeKHR *present_modes;
+    u32 present_mode_count;
+}dgSwapChainSupportDetails;
+
+static void dg_swapchain_support_details_free(dgSwapChainSupportDetails *s)
+{
+    if (s->present_modes)free(s->present_modes);
+    if (s->formats)free(s->formats);
+}
+
+static dgSwapChainSupportDetails dg_query_swapchain_support(VkPhysicalDevice device, VkSurfaceKHR surface)
+{
+    dgSwapChainSupportDetails details = {0};
+    
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+    
+    u32 format_count;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &format_count, NULL);
+    details.format_count = format_count;
+    
+    if (format_count != 0)
+    {
+        details.formats = (VkSurfaceFormatKHR*)malloc(sizeof(VkSurfaceFormatKHR) * format_count);
+        vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &format_count, details.formats);
+    }
+    u32 present_mode_count;
+    vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &present_mode_count, NULL);
+    details.present_mode_count = present_mode_count;
+    
+    if (present_mode_count != 0)
+    {
+        details.present_modes = (VkPresentModeKHR*)malloc(sizeof(VkPresentModeKHR) * present_mode_count);
+        vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &present_mode_count, details.present_modes);
+    }
+    
+    
+    return details;
+}
+static u32 is_device_suitable(VkPhysicalDevice device, VkSurfaceKHR surface)
+{
+	dgQueueFamilyIndices indices = dg_find_queue_families(device);
     
     VkPhysicalDeviceProperties device_properties;
     VkPhysicalDeviceFeatures device_features;
@@ -156,11 +202,11 @@ static u32 is_device_suitable(VkPhysicalDevice device)
     u32 extensions_supported = dg_check_device_extension_support(device);
     if (extensions_supported == 0) printf("GRAPHICS DRIVER: some device extension not supported!!\n");
     
-    //dSwapChainSupportDetails swapchain_support = query_swapchain_support(device);
+    dgSwapChainSupportDetails swapchain_support = dg_query_swapchain_support(device, surface);
     
     //here we can add more requirements for physical device selection
-    return indices.graphics_family_found && extensions_supported && indices.present_family_found;
-    //(swapchain_support.format_count > 0) && (swapchain_support.present_mode_count > 0);	
+    return indices.graphics_family_found && extensions_supported && indices.present_family_found
+    &&(swapchain_support.format_count > 0) && (swapchain_support.present_mode_count > 0);	
 }
 
 b32 dg_pick_physical_device(dgDevice *ddev)
@@ -178,7 +224,7 @@ b32 dg_pick_physical_device(dgDevice *ddev)
     vkEnumeratePhysicalDevices(ddev->instance, &device_count, devices);
 	//@FIX(ilias): this is 1 here because llvmpipe is 0 and we don't want that! no vulkan 1.3!!
     for (u32 i = 1; i < device_count; ++i)
-        if (is_device_suitable(devices[i]))
+        if (is_device_suitable(devices[i], ddev->surface))
 			{
 				ddev->physical_device = devices[i];
 
@@ -201,7 +247,7 @@ b32 dg_surface_create(dgDevice *ddev,dWindow *window)
 
 b32 dg_create_logical_device(dgDevice *ddev)
 {
-    dQueueFamilyIndices indices = dg_find_queue_families(ddev->physical_device);
+    dgQueueFamilyIndices indices = dg_find_queue_families(ddev->physical_device);
     
     f32 queue_priority = 1.0f;
 	VkDeviceQueueCreateInfo queue_create_info[2] = {0};
@@ -249,8 +295,135 @@ b32 dg_create_logical_device(dgDevice *ddev)
 	return TRUE;
 }
 
-//NOTE(ilias): This is UGLY AF!!!!
-extern dWindow main_window;
+static VkSurfaceFormatKHR dg_choose_swap_surface_format(dgSwapChainSupportDetails details)
+{
+    for (u32 i = 0; i < details.format_count; ++i)
+    {
+        if (details.formats[i].format == VK_FORMAT_B8G8R8A8_SRGB  &&
+            details.formats[i].colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+        {
+            return details.formats[i];
+        }
+    }
+    return details.formats[0];
+}
+
+static VkPresentModeKHR dg_choose_swap_present_mode(dgSwapChainSupportDetails details)
+{
+    return VK_PRESENT_MODE_FIFO_KHR;
+    //return VK_PRESENT_MODE_IMMEDIATE_KHR;
+}
+
+static VkExtent2D dg_choose_swap_extent(dgSwapChainSupportDetails details)
+{
+    if (details.capabilities.currentExtent.width != UINT32_MAX)
+    {
+        return details.capabilities.currentExtent;
+    }
+    else
+    {
+        u32 width, height;
+        width = main_window.width;
+        height = main_window.height;
+
+        VkExtent2D actual_extent = {width, height};
+        actual_extent.height=maximum(details.capabilities.maxImageExtent.height,
+                                     minimum(details.capabilities.minImageExtent.height,actual_extent.height));
+        actual_extent.width=maximum(details.capabilities.maxImageExtent.width,
+                                    minimum(details.capabilities.minImageExtent.width,actual_extent.width));
+        return actual_extent;
+    }
+}
+
+#define MAX_SWAP_IMAGE_COUNT 2
+static b32 dg_create_swapchain(dgDevice *ddev)
+{
+
+    dgSwapChainSupportDetails swap_details = dg_query_swapchain_support(ddev->physical_device, ddev->surface);
+
+    VkSurfaceFormatKHR surface_format = dg_choose_swap_surface_format(swap_details);
+    VkPresentModeKHR present_mode = dg_choose_swap_present_mode(swap_details);
+    VkExtent2D extent = dg_choose_swap_extent(swap_details);
+
+    u32 image_count = swap_details.capabilities.minImageCount + 1;
+    if (swap_details.capabilities.maxImageCount > 0 && image_count > swap_details.capabilities.maxImageCount)
+        image_count = swap_details.capabilities.maxImageCount;
+    image_count = minimum(image_count, MAX_SWAP_IMAGE_COUNT);
+
+    VkSwapchainCreateInfoKHR create_info = {0};
+    create_info.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    create_info.surface = ddev->surface;
+
+    create_info.minImageCount = image_count;
+    create_info.imageFormat = surface_format.format;
+    create_info.imageColorSpace = surface_format.colorSpace;
+    create_info.imageExtent = extent;
+    create_info.imageArrayLayers = 1;
+    create_info.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+    dgQueueFamilyIndices indices = dg_find_queue_families(ddev->physical_device);
+    u32 queue_family_indices[] = {indices.graphics_family, indices.present_family};
+    
+    if (indices.graphics_family != indices.present_family)
+    {
+        create_info.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+        create_info.queueFamilyIndexCount = 2;
+        create_info.pQueueFamilyIndices = queue_family_indices;
+    }
+    else
+    {
+        create_info.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        create_info.queueFamilyIndexCount = 0;
+        create_info.pQueueFamilyIndices = NULL;
+    }
+    create_info.preTransform = swap_details.capabilities.currentTransform;
+    create_info.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    create_info.presentMode = present_mode;
+    create_info.clipped = VK_TRUE;
+    create_info.oldSwapchain = VK_NULL_HANDLE;
+
+    VK_CHECK(vkCreateSwapchainKHR(ddev->device, &create_info, NULL, &ddev->swap.swapchain));
+
+    vkGetSwapchainImagesKHR(ddev->device, ddev->swap.swapchain, &image_count, NULL);
+    ddev->swap.images = (VkImage*)malloc(sizeof(VkImage) * image_count);
+    vkGetSwapchainImagesKHR(ddev->device, ddev->swap.swapchain, &image_count, ddev->swap.images);
+    ddev->swap.image_format = surface_format.format;
+    ddev->swap.extent = extent;
+    ddev->swap.image_count = image_count;//TODO(ilias): check
+    printf("new swapchain image_count: %i\n", image_count);
+    printf("new swapchain image_dims: %i %i\n", ddev->swap.extent.width, ddev->swap.extent.height);
+	//ddev->swap.depth_attachment = dg_create_depth_attachment(ddev->swap.extent.width, ddev->swap.extent.height);
+	
+	//ddev->swap.rp_begin = create_render_pass(VK_ATTACHMENT_LOAD_OP_CLEAR,VK_IMAGE_LAYOUT_UNDEFINED,VK_IMAGE_LAYOUT_GENERAL, 1, TRUE);
+
+    return DSUCCESS;
+}
+
+static VkImageView dg_create_image_view(VkImage image, VkFormat format,  VkImageAspectFlags aspect_flags)
+{
+	VkImageView image_view;
+	
+	VkImageViewCreateInfo view_info = {0};
+	view_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+	view_info.image = image;
+	view_info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+	view_info.format = format;
+	view_info.subresourceRange.aspectMask = aspect_flags;
+	view_info.subresourceRange.baseMipLevel = 0;
+	view_info.subresourceRange.levelCount = 1;
+	view_info.subresourceRange.baseArrayLayer = 0;
+	view_info.subresourceRange.layerCount = 1;
+	VK_CHECK(vkCreateImageView(dd.device, &view_info, NULL, &image_view));
+	return image_view;
+}
+
+static b32 dg_create_swapchain_image_views(dgDevice *ddev)
+{
+    ddev->swap.image_views = (VkImageView*)malloc(sizeof(VkImageView) * ddev->swap.image_count);
+    for (u32 i = 0; i < ddev->swap.image_count; ++i)
+		ddev->swap.image_views[i] = dg_create_image_view(ddev->swap.images[i], ddev->swap.image_format,VK_IMAGE_ASPECT_COLOR_BIT);
+    return DSUCCESS;
+}
 
 void dg_device_init(void)
 {
@@ -258,6 +431,9 @@ void dg_device_init(void)
 	assert(dg_surface_create(&dd,&main_window));
 	assert(dg_pick_physical_device(&dd));
 	assert(dg_create_logical_device(&dd));
+    assert(dg_create_swapchain(&dd));
+    assert(dg_create_swapchain_image_views(&dd));
+
 	printf("Vulkan initialized correctly!\n");
 }
 
--- src/dgfx.h
+++ src/dgfx.h
@@ -6,6 +6,33 @@
 
 #define DG_PHYSICAL_DEVICE_MAX 10
 
+
+typedef struct dgTexture{
+    VkSampler sampler;
+    VkImage image;
+    VkImageLayout image_layout;
+    VkDeviceMemory mem;
+    VkImageView view;
+    VkFormat format;
+    u32 width, height;
+    u32 mip_levels;
+} dgTexture;
+
+typedef struct dgSwapchain
+{ 
+	VkSwapchainKHR swapchain;
+	VkImage *images;
+	VkFormat image_format;
+	VkExtent2D extent;
+	VkImageView *image_views;
+	VkFramebuffer *framebuffers;
+	dgTexture depth_attachment;
+	u32 image_count;
+	
+    //TODO(ilias): delete render pass, its the LAW!!!!!!!!!!
+	VkRenderPass rp_begin;
+}dgSwapchain;
+
 typedef struct dgDevice
 {
     VkInstance instance;
@@ -16,6 +43,8 @@ typedef struct dgDevice
 
     VkQueue graphics_queue;
     VkQueue present_queue;
+
+    dgSwapchain swap;
 
     u32 image_index; //current image index to draw
 }dgDevice;
